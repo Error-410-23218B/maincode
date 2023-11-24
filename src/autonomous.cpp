37b33777f9050c07e738e35ef242b2a96e4841a8
Langton-Vex <19rjose@thelangton.org.uk>
2023-11-24 17:28:38 +0000

    sdsdfsdfef
    sdfsdsf

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -21,18 +21,28 @@ void pneumFA(){
 }
 
 void skillAuton(){
-  puncherMotorGroup.setVelocity(70, percent);
-  puncherMotorGroup.setMaxTorque(100,percent);  
-  puncherMotorGroup.spin(forward);
-  wait(40,sec);
-  puncherMotorGroup.stop();
-  Drivetrain.driveFor(reverse,6,inches);
-  Drivetrain.turnFor(right,180,deg);
-  Drivetrain.driveFor(forward,25,inches);
-  Drivetrain.turnFor(right,90,deg);
-  Drivetrain.driveFor(forward,36,inches);
+  // puncherMotorGroup.setVelocity(70, percent);
+  // puncherMotorGroup.setMaxTorque(100,percent);  
+  // puncherMotorGroup.spin(forward);
+  // wait(40,sec);
+  // puncherMotorGroup.stop();
+  Drivetrain.driveFor(forward,5,inches);
+  Drivetrain.turnFor(left,125,deg);  
+    Drivetrain.driveFor(forward,100,inches);
+    Drivetrain.turnFor(right,60,degrees);
+  Drivetrain.driveFor(forward,10,inches);
+  Drivetrain.turnFor(right,90,degrees);
   pneumA();
-  Drivetrain.driveFor(forward,30,inches);
+  Drivetrain.driveFor(forward,70,inches);
+  
+
+
+
+  // Drivetrain.turnFor(right,135,deg);
+  // Drivetrain.driveFor(forward,36,inches);
+  // pneumA();
+  // Drivetrain.driveFor(forward,30,inches);
+  
 }
 
 void basicAuton(){
@@ -56,42 +66,41 @@ void secondBasicAuton(){
   Drivetrain.driveFor(forward,50,inches);
   Drivetrain.turnFor(right,180,degrees);
   Drivetrain.driveFor(forward,65,inches);
-
 }
 
-void skillAuton(){
-  Drivetrain.setDriveVelocity(95,percent);
-  Drivetrain.driveFor(forward,20,inches);
-  Drivetrain.turnFor(left,130,deg);
-  Drivetrain.driveFor(reverse,16,inches);
-  puncherMotorGroup.setVelocity(60, percent);
-  puncherMotorGroup.setMaxTorque(100,percent);  
-  puncherMotorGroup.spin(forward);
-  wait(30,sec);
-  puncherMotorGroup.stop();
-  Drivetrain.turnFor(right,95,degrees);
-  // Drivetrain.setStopping(hold);
-  Drivetrain.driveFor(forward,114,inches);
-  Drivetrain.turnFor(left,140,degrees);
-  Drivetrain.driveFor(reverse,50,inches);
-    Drivetrain.setDriveVelocity(200,percent);
-  Drivetrain.driveFor(forward,210,inches);
-    Drivetrain.setDriveVelocity(95,percent);
-    Drivetrain.turnFor(right,40,degrees);
-pneumA();
-// Drivetrain.driveFor(forward,90,inches);
+// void skillAuton(){
+//   Drivetrain.setDriveVelocity(95,percent);
+//   Drivetrain.driveFor(forward,20,inches);
+//   Drivetrain.turnFor(left,130,deg);
+//   Drivetrain.driveFor(reverse,16,inches);
+//   puncherMotorGroup.setVelocity(60, percent);
+//   puncherMotorGroup.setMaxTorque(100,percent);  
+//   puncherMotorGroup.spin(forward);
+//   wait(30,sec);
+//   puncherMotorGroup.stop();
+//   Drivetrain.turnFor(right,95,degrees);
+//   // Drivetrain.setStopping(hold);
+//   Drivetrain.driveFor(forward,114,inches);
+//   Drivetrain.turnFor(left,140,degrees);
+//   Drivetrain.driveFor(reverse,50,inches);
+//     Drivetrain.setDriveVelocity(200,percent);
+//   Drivetrain.driveFor(forward,210,inches);
+//     Drivetrain.setDriveVelocity(95,percent);
+//     Drivetrain.turnFor(right,40,degrees);
+// pneumA();
+// // Drivetrain.driveFor(forward,90,inches);
 
-  // Drivetrain.driveFor(forward,25,inches);
-  // Drivetrain.turnFor(right,90,deg);
-  // Drivetrain.driveFor(forward,36,inches);
-  // pneumA();
-  // Drivetrain.driveFor(forward,30,inches);
-  // Drivetrain.driveFor(reverse,60,inches);
-  // Drivetrain.turnFor(right,225,degrees);
-  // Drivetrain.driveFor(forward,40,inches);
-  // pneumA();
-  // Drivetrain.driveFor(forward,90,inches);
-}
+//   // Drivetrain.driveFor(forward,25,inches);
+//   // Drivetrain.turnFor(right,90,deg);
+//   // Drivetrain.driveFor(forward,36,inches);
+//   // pneumA();
+//   // Drivetrain.driveFor(forward,30,inches);
+//   // Drivetrain.driveFor(reverse,60,inches);
+//   // Drivetrain.turnFor(right,225,degrees);
+//   // Drivetrain.driveFor(forward,40,inches);
+//   // pneumA();
+//   // Drivetrain.driveFor(forward,90,inches);
+// }
 
 void autonomous(){
   // task pidControlTask(voltControl);
